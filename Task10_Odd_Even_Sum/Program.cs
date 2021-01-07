9fdfed12fc32454a04354a0791a78574f8c62f74
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 19:47:34 +0200

    Add files via upload

---
+++ Task10_Odd_Even_Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task10_Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int nextDigit = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum += nextDigit;
+                }
+                else
+                {
+                    oddSum += nextDigit;
+                }
+            }
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(evenSum - oddSum)}");
+            }
+        }
+    }
+}
