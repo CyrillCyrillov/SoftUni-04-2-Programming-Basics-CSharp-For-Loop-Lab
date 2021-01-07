90f7c1ffa4028efc0fe9b97b125e2e1897a5f532
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 19:43:22 +0200

    Add files via upload

---
+++ Task05_Character_Sequence/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task05_Character_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int symbols = word.Length;
+
+            for (int i = 0; i < symbols; i++)
+            {
+                char letter = word[i];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
