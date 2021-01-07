87a6c1c1e80b9d7822c020828727bc72697755d6
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 19:45:05 +0200

    Add files via upload

---
+++ Task07_Sum_Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task07_Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= numbers; i++)
+            {
+                int nextDigit = int.Parse(Console.ReadLine());
+                sum = sum + nextDigit;
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
