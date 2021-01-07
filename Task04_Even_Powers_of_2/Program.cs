b90013ad0adf058bacb045e13c64fe207d2ffc32
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 19:42:27 +0200

    Add files via upload

---
+++ Task04_Even_Powers_of_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task04_Even_Powers_of_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double number = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    number = Math.Pow(2, i);
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
