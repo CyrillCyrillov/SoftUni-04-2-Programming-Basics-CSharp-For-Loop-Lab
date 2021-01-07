885276bb29ac10d7064e335007907607a2849606
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 19:45:52 +0200

    Add files via upload

---
+++ Task08_Number_sequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task08_Number_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 0; i < numbers; i++)
+            {
+                int nextDigit = int.Parse(Console.ReadLine());
+                if (nextDigit >= max)
+                {
+                    max = nextDigit;
+                }
+               if (nextDigit <= min)
+                {
+                    min = nextDigit;
+                }
+            }
+            
+            Console.WriteLine($"Max number: {max}");
+            Console.WriteLine($"Min number: {min}");
+        }
+    }
+}
