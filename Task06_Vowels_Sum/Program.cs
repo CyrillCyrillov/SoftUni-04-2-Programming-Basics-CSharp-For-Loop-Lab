1887b4ff091d172dbe180cfa2c6f32d19f97a9e9
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 19:44:12 +0200

    Add files via upload

---
+++ Task06_Vowels_Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task06_Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int n = word.Length;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                char letter = word[i];
+                if (letter == 'a')
+                {
+                    sum = sum + 1;
+                }
+                else if (letter == 'e')
+                {
+                    sum = sum + 2;
+                }
+                else if (letter == 'i')
+                {
+                    sum = sum + 3;
+                }
+                else if (letter == 'o')
+                {
+                    sum = sum + 4;
+                }
+                else if (letter == 'u')
+                {
+                    sum = sum + 5;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
